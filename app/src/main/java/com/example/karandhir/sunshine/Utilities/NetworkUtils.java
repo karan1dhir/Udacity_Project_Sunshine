d250bc2f8b3cd1a8c374def1061a82f91a6b47d3
SoloWarrior <karandhir64@yahoo.co.in>
2018-12-25 15:36:11 +0530

    basic setup

---
+++ app/src/main/java/com/example/karandhir/sunshine/Utilities/NetworkUtils.java
@@ -0,0 +1,69 @@
+package com.example.karandhir.sunshine.Utilities;
+
+import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class NetworkUtils {
+
+
+    private static final String DYNAMIC_WEATHER_URL ="https://andfun-weather.udacity.com/weather";
+    private static final String STATIC_WEATHER_URL ="https://andfun-weather.udacity.com/staticweather";
+    private static final String FORECAST_BASE_URL = STATIC_WEATHER_URL;
+    private static final String format = "json";
+    private static final String units = "metric";
+    private static final int numDays = 14;
+    final static String QUERY_PARAM = "q";
+    final static String LAT_PARAM = "lat";
+    final static String LON_PARAM = "lon";
+    final static String FORMAT_PARAM = "mode";
+    final static String UNITS_PARAM = "units";
+    final static String DAYS_PARAM = "cnt";
+
+
+    public static URL buildURL(String locationQuery)  {
+
+        Uri uri = Uri.parse(DYNAMIC_WEATHER_URL).buildUpon()
+                .appendQueryParameter(QUERY_PARAM,locationQuery)
+                .appendQueryParameter(FORMAT_PARAM,format)
+                 .appendQueryParameter(UNITS_PARAM,units)
+                .appendQueryParameter(DAYS_PARAM,Integer.toString(numDays))
+                .build();
+        URL url = null;
+        try {
+            url = new URL(url.toString());
+        }
+        catch (MalformedURLException ex){
+            ex.printStackTrace();
+        }
+        return url;
+    }
+
+    public static URL buildURL(Double lat,Double lon){
+        return null;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
